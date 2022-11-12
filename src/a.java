9dd1d0a260b73d159115d3c33d1c86deb91e4775
cartonHouse <2873947898@qq.com>
2022-11-12 23:43:19 +0800

    commit src file

---
+++ src/a.java
@@ -0,0 +1,11 @@
+public class a{
+	public static void main(String[] args){
+		System.out.println("It is a");
+	}
+}
+class b
+{
+	public static void main(String[] args){
+		System.out.println("It is b");
+	}
+}
